b41ef3c8e06b2c8ef079bba3e28f941eafe9ead3
Albert <zan15051@gmail.com>
2023-05-02 22:25:55 +0700

    Implemented simple function solver logic.
    I don't know if it works.

---
+++ Solver/ViewModels/FunctionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver.ViewModels
+{
+    public partial class FunctionViewModel : ObservableObject
+    {
+        [ObservableProperty] private double _x;
+        [ObservableProperty] private double _y;
+        [ObservableProperty] private double _a;
+        [ObservableProperty] private double _b;
+        [ObservableProperty] private double _c;
+        [ObservableProperty] private double _power;
+
+        public double Result => _a * Math.Pow(_x, _power) + _b * Math.Pow(_y, _power - 1) + _c;
+        public string Title { get; set; }
+        public Function(string title, int power)
+        {
+            Title = title;
+            _power = power;
+        }
+
+
+
+    }
+}
---
+++ Solver/ViewModels/SolverViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Solver.ViewModels;
+
+namespace Solver.ViewModels
+{
+    public partial class SolverViewModel : ObservableObject
+    {
+        [ObservableProperty] private readonly Dictionary<int, FunctionViewModel> _functions = new Dictionary<int, FunctionViewModel>()
+        {
+            { 1, new FunctionViewModel("Линейная", 1) },
+            { 2, new FunctionViewModel("Квадратичная", 2) },
+            { 3, new FunctionViewModel("Кубическая", 3) },
+            { 4, new FunctionViewModel("4-й степени", 4) },
+            { 5, new FunctionViewModel("5-й степени", 5) },
+        };
+    }
+}
