12dda6769b8f0ab9be90c7b0916f990b23739dee
Albert <zan15051@gmail.com>
2023-05-03 00:16:43 +0700

    Changed x and y fields to collection with type tuple.
    It was done beacause there could be multiple of x and y values and we
    need to have and store results (f(x,y)) for all of them. For storing the
    results Result, get-only property is now also a collection.

--- Solver/ViewModels/FunctionViewModel.cs
+++ Solver/ViewModels/FunctionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,24 @@ namespace Solver.ViewModels
 {
     public partial class FunctionViewModel : ObservableObject
     {
-        [ObservableProperty] private double _x;
-        [ObservableProperty] private double _y;
-        [ObservableProperty] private double _a;
-        [ObservableProperty] private double _b;
-        [ObservableProperty] private double _c;
-        [ObservableProperty] private double _power;
+        [ObservableProperty] private ObservableCollection<(double x, double y)> _valuesXY;
+        [ObservableProperty] private double _a = 0;
+        [ObservableProperty] private double _b = 0;
+        [ObservableProperty] private double _c = 0;
+        [ObservableProperty] private double _power = 1;
         [ObservableProperty] private string _title;
 
-        public double Result => _a * Math.Pow(_x, _power) + _b * Math.Pow(_y, _power - 1) + _c;
+        public ObservableCollection<double> Results
+        {
+            get
+            {
+                foreach (var item in _valuesXY)
+                {
+                    Results.Add(_a* Math.Pow(item.x, _power) + _b * Math.Pow(item.y, _power - 1) + _c);
+                }
+                
+            }
+        }
         public FunctionViewModel(string title, int power)
         {
             Title = title;
